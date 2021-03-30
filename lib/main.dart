f65b8484112233f235902f8e9c655b3ec7e7bffa
Ron Nahmias <ron.nahmias2@gmail.com>
2021-03-31 01:06:40 +0300

    more fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:chat_app/screens/auth_screen.dart';
-import 'package:chat_app/screens/chat_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
